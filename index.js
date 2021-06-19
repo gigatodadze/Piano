43e3842042bcf7890a7e5279bbdfcd64f32cba8f
gigatodadze <72299807+gigatodadze@users.noreply.github.com>
2021-06-19 15:55:24 +0400

    getting the child key!

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
-    $("#key1").on("click", function() {
-        var audio = $("#piano1")[0];
-        audio.play();
+    var idArray = [];
+    $('.piano-key').each(function () {
+        idArray.push(this.id);
     });
+    console.log(idArray)
+
+    $(".piano-key").on("click", function (event) {
+
+        console.log(event.target.id);
+        var selected = $("#"+event.target.id).children("audio").attr("id");
+
+        console.log(selected);
+
+        $(event.target.id).find("audio").each(function (){
+        console.log("1")
+            var audio = $(this).attr('id')[0]
+            audio.play();
+        });
+
+
+        // console.log(event.target.id,idArray.length);
+        //     $(event.target.id).on("click", function () {
+        //         var audio = $("#key1")[0];
+        //         audio.play();
+        //     });
+    })
 });
